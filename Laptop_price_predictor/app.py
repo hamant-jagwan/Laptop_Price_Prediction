886caef5f7c9aef15ad140dc8aa5fdf416dc21d0
Hamant Jagwan <Hemant7jagwan@gmail.com>
2025-03-27 23:43:50 +0530

    updating app.py file

--- Laptop_price_predictor/app.py
+++ Laptop_price_predictor/app.py
@@ -16,3 +16,38 @@ company = st.selectbox('Brand', df['Company'].unique())
 
 # type of Laptop
 type = st.selectbox('Type', df['TypeName'].unique())
+
+# Ram
+type = st.selectbox('Ram(in GB)', [2,4,6,8,16,24,32,64])
+
+# Weight
+weight = st.number_input('Weight of the Laptop')
+
+# touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# Screen Size
+screen_size = st.number_input('Screen size')
+
+# Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080','1366x768','1600x900','1340x2160',
+                                                '3200x1800','2880x1800','2560x1600','2560x1440',
+                                                '2304x1440',])
+
+# Cpu
+cpu = st.selectbox('CPU', df['Cpu_brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD(in GB)', [0,8,128, 255, 512, 1024, 2048])
+
+ssd = st.selectbox('SSD(in GB)', [0,8,128, 255, 512, 1024, 2048])
+
+gpu = st.selectbox('GPU', df['Gpu_brand'].unique())
+
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    pass
